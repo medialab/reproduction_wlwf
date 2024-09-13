5c3f72557d269deec6756eeff22236bc20813cef
bmazoyer <beatrice.mazoyer@sciencespo.fr>
2024-09-13 12:29:19 +0000

    Save bertopic

--- 02_run_bertopic.py
+++ 02_run_bertopic.py
@@ -1,77 +1,77 @@
+import sys
+import json
 import time
 import random
 
+from hdbscan import HDBSCAN
 from bertopic import BERTopic
+import bertopic._save_utils as save_utils
 import numpy as np
 import matplotlib.pyplot as plt
 
-from utils import TWEETS_FOLDER, count_nb_files, vectorizer, preprocess
+from utils import count_nb_files, vectorizer, preprocess, SBERT_NAME
 
-def st_time(func):
-    """
-        decorator to compute the total time of a function
-    """
-
-    def st_func(*args, times=[]):
-        t1 = time.time()
-        r = func(*args, times=times)
-        t2 = time.time()
-        print("Function=%s, Time=%s" % (func.__name__, t2 - t1))
-        times.append(t2 - t1)
-        return r
-
-    return st_func
-
-
-@st_time
-def compute_topics(docs, embeddings, times=[]):
-    print("Nb documents: ", len(docs))
-    topics, probs = topic_model.fit_transform(docs, embeddings)
-    print(topic_model.get_topic_info())
-
+hdbscan_model = HDBSCAN(min_cluster_size=3, metric='euclidean', cluster_selection_method='eom', prediction_data=False)
 
 topic_model = BERTopic(
-
-  # Pipeline models
-  vectorizer_model=vectorizer,
-
-  # Hyperparameters
-  top_n_words=10,
-  verbose=True
-
+    vectorizer_model=vectorizer,
+    hdbscan_model=hdbscan_model,
+    nr_topics=100,
+    # Hyperparameters
+    top_n_words=10,
+    verbose=True,
 )
 
 
-def plot_time(x, y):
-    fig, ax = plt.subplots()
-    ax.plot(x, y)
+class NpEncoder(json.JSONEncoder):
+    """From https://stackoverflow.com/a/57915246/6053864"""
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+
+def save_ctfidf_config(model, path):
+    """Save parameters to recreate CountVectorizer and c-TF-IDF."""
+    config = {}
+
+    # Recreate ClassTfidfTransformer
+    config["ctfidf_model"] = {
+        "bm25_weighting": model.ctfidf_model.bm25_weighting,
+        "reduce_frequent_words": model.ctfidf_model.reduce_frequent_words,
+    }
+
+    # Recreate CountVectorizer
+    cv_params = model.vectorizer_model.get_params()
+    del cv_params["tokenizer"], cv_params["preprocessor"], cv_params["dtype"]
+    if not isinstance(cv_params["analyzer"], str):
+        del cv_params["analyzer"]
+
+    config["vectorizer_model"] = {
+        "params": cv_params,
+        "vocab": model.vectorizer_model.vocabulary_,
+    }
     
-    ax.set(xlabel='nb_docs', ylabel='time (s)')
-    ax.grid()
-    
-    fig.savefig("plots/bertopic_time.png")
-    plt.show()
+    with path.open("w") as f:
+        json.dump(config, f, indent=2, cls=NpEncoder)
+
+save_utils.save_ctfidf_config = save_ctfidf_config
+
+path = sys.argv[1]
+docs = preprocess(path, count_nb_files(path))
+load_path = "data_prod/embeddings/tweets_from_deputesXVI_220620-230313_sentence-camembert-large.npz"
 
 
-
-docs = preprocess(TWEETS_FOLDER)
-save_path = "data/embeddings/tweets_from_deputesXVI_220620-230313_sentence-camembert-large.npz"
-
-embeddings = np.load(save_path)["embeddings"]
+embeddings = np.load(load_path)["embeddings"]
 
 docs = np.array(docs)
 
-sizes = [1_000, 10_000, 100_000, len(docs)]
-times = []
+topic_model.fit(docs, embeddings)
 
-for k in [1_000, 10_000, 100_000, len(docs)]:
-    if k == len(docs):
-        sampled_idx = range(len(docs))
-    else:
-        sampled_idx = random.sample(range(1, len(docs)), k)
-    sampled_docs = docs[sampled_idx]
-    sampled_embeddings = embeddings[sampled_idx]
-    
-    compute_topics(sampled_docs, sampled_embeddings, times=times)
+#Save model
+topic_model.save("data_prod/bertopic/fit_on_deputesXVI_220620-230313_sentence-camembert-large", serialization="safetensors", save_ctfidf=True, save_embedding_model=SBERT_NAME)
 
-plot_time(sizes, times)
