9f30113ce1a55e0cc4757dc13918c97ffb34d68c
Antoine <antoine.machut@umrpacte.fr>
2024-11-06 12:00:20 +0100

    Add "requirement.R" script to conditionally install missing packages

---
+++ 00_requirement.R
@@ -0,0 +1,41 @@
+#===============================================================================
+#  File-Name:	00-requirements.R
+#  Date:	May 6, 2019
+#  Paper:   Who Leads? Who Follows? Measuring Issue Attention and Agenda Setting
+#           by Legislators and the Mass Public Using Social Media Data
+#  Journal: American Political Science Review
+#  Authors: Pablo Barbera, Andreu Casas, Jonathan Nagler, Patrick J. Egan,
+#           Richard Bonneau, John Jost, Joshua A. Tucker
+#  Purpose: check if required packages for the following R scripts exist and install it if needed
+#  Data In:
+#           ./data/tweets/text/* & var/lda_results-twokenizer.Rdata &
+#           var/lda-ouput/*
+#===============================================================================
+
+install_if_missing <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg)
+  }
+}
+
+packages <- c(
+  "cvTools",
+  "ggdendro",
+  "ggplot2",
+  "grid",
+  "gridExtra",
+  "gtable",
+  "ggthemes",
+  "Matrix",
+  "reshape",
+  "slam",
+  "scales",
+  "shiny",
+  "tidyverse",
+  "tm",
+  "topicmodels"
+)
+
+invisible(lapply(packages, 
+                 install_if_missing)
+          )
