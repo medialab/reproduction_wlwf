909c6bfa1e1c0869ce3338123c3ef30a3613b06e
jmeynent <fynch.meynent@sciencespo.fr>
2025-01-27 17:25:43 +0000

    Update data structuration and R script for VAR

--- 05a_Structure_data_for_VAR.py
+++ 05a_Structure_data_for_VAR.py
@@ -26,37 +26,40 @@ with open('data_prod/var/bertopic/general_TS.csv') as f:
 
 for topic in topics:
     topics_files = files_ts[count_topic, count_topic + 5]
-    reader0 = topics_files[0] #attentive
+    reader0 = topics_files[0] #attentive 
     reader1 = topics_files[1] #deputes
     reader2 = topics_files[2] #general
     reader3 = topics_files[3] #medias
-    reader4 = topics_files[4] #supporters 
-    
+    reader4 = topics_files[4] #supporters
 
-    for date in 
+    date_col = reader0.headers.date
+    prop_col_index = reader0.headers.prop
+
+    iter_deputes = (row[prop_col_index] for row in reader1)
+    iter_supp = (row[prop_col_index] for row in reader4)
+
+    for row1, row2, row3 in zip(reader0, reader2, reader3):
+        writer.writerow({
+                    'date': row1[date_col],
+                    'topic': topic_number,
+                    'lr': next(iter_deputes),
+                    'majority': next(iter_deputes),
+                    'nupes': next(iter_deputes),
+                    'rn': next(iter_deputes),
+                    'lr_supp': next(iter_supp),
+                    'majority_supp': next(iter_supp),
+                    'nupes_supp': next(iter_supp),
+                    'rn_supp': next(iter_supp),
+                    'attentive_public': row1[prop_col_index],
+                    'general_public': row2[prop_col_index],
+                    'medias': row3[prop_col_index]
+                })
 
     count_topic += 5 #To obtain the files of the next topics next time
 
-for file in files_TS:
-
-    filename = os.path.basename(file)
-    topic_number = re.search(r'-?\d+', filename)[0]
-    reader = casanova.reader(open(file))
-
-    for row in reader:
-        if count_iter ==0: #Create date column one time
-            casanova.writer()
-            count_first += 1
-
-
 
 '''
-
-Récupérer tous les fichiers csv 
-Les reformer pour avoir la grosse data base générale
-Feature à mettre : ne pas prendre en compte le fichier général créé dans les calculs ``
-
-Note : il faudra aussi sorted par date les supporters lorsque je pourrai pull après le pull request de BERTopic3
+Note : il faudra aussi sorted par date les deputes supporters lorsque je pourrai pull après le pull request de BERTopic3
 
 Piste : ajout de party_day_counts = sorted(party_day_counts, key=lambda x: x[2]) dans script 02 ligne 217 pour trier par date et non plus pas parti les time series, mais des mofig
 Voir salade avec béatrice pour gérer ça (27/1)'''
--- 05b-var-model.r
+++ 05b-var-model.r
@@ -5,23 +5,55 @@ library(ggplot2)
 library(vars)
 library(boot)
 library(rio)
+library(tseries)
 
-#Importer les bases de données
-# Définir le chemin du dossier contenant les fichiers CSV
-folder_path <- "data_prod/dashboard/bertopic/data" #Modifier bertopic par lda si on souhaite faire les VAR sur LDA. Voir aussi la gestion des noms de fichier
+#Put our main databse generated thanks to script 05a 
+db <- read_csv("data_prod/var/bertopic/general_TS.csv")
 
-# Lister tous les fichiers CSV dans le dossier
-csv_files <- list.files(path = folder_path, pattern = "\\.csv$", full.names = TRUE)
-data_list <- lapply(csv_files, read.csv)
-names(data_list) <- basename(csv_files) #Nommer chaque base 
-
-#Il va falloir fusionner ces listes. Il faudra adapter les colonnes en conséquence
-#But : avoir une base de la force : date/topic/party or group/prop. We will call it db 
-
-#Conserver uniquement les topics politiques 
+#Keep only political topics
 pol_issues <- c() #Insert numbers
 
 db <- db %>%
   filter(topic %in% pol_issues)
 
 #variables <- c("lr", "majority", "nupes", "rn", "publr", "pubmajority",, "pubnupes", "pubrn", "att_public", "gen_public", "media")
+
+variables <- c('lr', 'majority', 'nupes', 'rn', 'lr_supp', 'majority_supp', 'nupes_supp', 'rn_supp', 'attentive_public', 'general_public', 'medias')
+
+# - logit transform all series
+for (v in variables) {
+  # - pulling the series-agenda for that group
+  x <- db[,v]
+  # - for some groups the last couple observations for each issues are NA,
+  #     making these a 0 
+  x[which(is.na(x))] <-0.01
+  # - adding 1 percentage point to avoid 0s before the logit transformation
+  #x <- x + 0.01
+  # - applying the non-linear transformation
+  logit_x <- log(x / (1-x))
+  db[,v] <- logit_x
+}
+
+#We add a Augmented Dickey-Fuller check so as to check time series stationarity
+cat("ADF Test for variable:", v, "\n")
+data <- db$v
+adf.test(data)
+
+maindb$topic <- as.character(maindb$topic)
+mformula <- formula(paste0("~", 
+                           paste0(variables, collapse = " + ")))
+model_data <- model.matrix(mformula, maindb[, variables])
+model_data <- model_data[, 2:ncol(model_data)] # removing intercept
+
+# - splitting the covariates of interest from the issue dummy variables
+X_endogenous <- model_data[, which(!grepl("topic", colnames(model_data)))]
+X_exogenous <- model_data[, which(grepl("topic", colnames(model_data)))]
+
+# - estimating the model: 7 lags
+var_model_merged <- VAR(y = X_endogenous, p = 7, exogen = X_exogenous)
+
+var_irfs_cum_merged <- irf(var_model_merged, n.ahead = 60, cumulative = TRUE)
+
+#Save
+save(var_model_merged, file = "data_prod/var/bertopic/var_model-MAIN.Rdata")
+save(var_irfs_cum_merged, file = "data_prod/var/bertopic/var_irfs-MAIN.Rdata")
