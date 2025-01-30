cd9ead53a20cd66e8d34db35c4b4386301fbf8b2
jmeynent <fynch.meynent@sciencespo.fr>
2025-01-30 13:21:32 +0000

    Finish 05a script to generate correct format for bertopic input

--- 05a_Structure_data_for_VAR.py
+++ 05a_Structure_data_for_VAR.py
@@ -4,62 +4,86 @@ The aim of this script is to create a database to an accomodating format for VAR
 
 import os
 import numpy as np 
+import csv
 import casanova
 import re
-from collections import default_dict
+import argparse
+from collections import defaultdict
+from datetime import datetime, timedelta
+from itertools import islice
 
 from utils import (
+    existing_dir_path,
     count_nb_files, 
     iter_on_files,
 )
 
+parser = argparse.ArgumentParser()
 
-files_TS =list((iter_on_files("data_prod/dashboard/bertopic/data"), count_nb_files("data_prod/dashboard/bertopic/data"))[1])
+parser.add_argument(
+    "--origin_path",
+    help="Path to an origin of your code",
+    type=existing_dir_path,
+    default=os.getcwd(), 
+)
 
-count_topic = 0
-nb_topics = 129 #Change this number the number of topics obtained with the model
-topics = (i-1 in range(nb_topics)) 
+args = parser.parse_args()
 
-with open('data_prod/var/bertopic/general_TS.csv') as f: 
-    writer = casanova.writer(f, fieldnames=['date', 'topic', 'lr', 'majority', 'nupes', 'rn', 'lr_supp', 'majority_supp', 'nupes_supp', 'rn_supp', 'attentive_public', 'general_public', 'medias'])
+input_path = os.path.join(args.origin_path, "data_prod/dashboard/bertopic/data")
+
+files_TS =list(iter_on_files(input_path, count_nb_files(input_path))[1]) 
+
+def count_dates(start_date, end_date):
+    if isinstance(start_date, str):
+        start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    if isinstance(end_date, str):
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    return (end_date - start_date).days + 1
+
+nb_dates = count_dates('2022-06-20', '2023-03-14')
+reader = casanova.reader(files_TS[0])
+group_types = list(dict.fromkeys(list(reader.cells('party'))))
+
+index_attentive = group_types.index('attentive')
+index_general = group_types.index('general')
+index_media = group_types.index('media')
+index_lrsupp = group_types.index('lr_supp')
+index_majsupp = group_types.index('majority_supp')
+index_nupessupp = group_types.index('nupes_supp')
+index_rnsupp = group_types.index('rn_supp')
+
+with open(os.path.join(args.origin_path, 'data_prod/var/bertopic/general_TS.csv'), 'w') as f: 
+    fieldnames = ['date', 'topic', 'lr', 'majority', 'nupes', 'rn', 'lr_supp', 'majority_supp', 'nupes_supp', 'rn_supp', 'attentive', 'general', 'media']
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
 
-for topic in topics:
-    topics_files = files_ts[count_topic, count_topic + 5]
-    reader0 = topics_files[0] #attentive 
-    reader1 = topics_files[1] #deputes
-    reader2 = topics_files[2] #general
-    reader3 = topics_files[3] #medias
-    reader4 = topics_files[4] #supporters
+    for file in files_TS:
+        with open(file, 'r') as file_reader:
+            reader = csv.DictReader(file_reader)
 
-    date_col = reader0.headers.date
-    prop_col_index = reader0.headers.prop
+            rows = list(reader)
+            iter_dates=0
 
-    iter_deputes = (row[prop_col_index] for row in reader1)
-    iter_supp = (row[prop_col_index] for row in reader4)
-
-    for row1, row2, row3 in zip(reader0, reader2, reader3):
-        writer.writerow({
-                    'date': row1[date_col],
-                    'topic': topic_number,
-                    'lr': next(iter_deputes),
-                    'majority': next(iter_deputes),
-                    'nupes': next(iter_deputes),
-                    'rn': next(iter_deputes),
-                    'lr_supp': next(iter_supp),
-                    'majority_supp': next(iter_supp),
-                    'nupes_supp': next(iter_supp),
-                    'rn_supp': next(iter_supp),
-                    'attentive_public': row1[prop_col_index],
-                    'general_public': row2[prop_col_index],
-                    'medias': row3[prop_col_index]
-                })
-
-    count_topic += 5 #To obtain the files of the next topics next time
+            for row in rows:
+                if iter_dates==nb_dates:
+                    break
+                writer.writerow({
+                        'date': rows[iter_dates*4]['date'],  
+                        'topic': row['topic'],
+                        'lr': rows[iter_dates]['prop'],
+                        'majority': rows[iter_dates + 1]['prop'],
+                        'nupes': rows[iter_dates + 2]['prop'],
+                        'rn': rows[iter_dates + 3]['prop'],
+                        'lr_supp': rows[iter_dates + index_lrsupp * nb_dates]['prop'],
+                        'majority_supp': rows[iter_dates + index_majsupp * nb_dates]['prop'],
+                        'nupes_supp': rows[iter_dates + index_nupessupp * nb_dates]['prop'],
+                        'rn_supp': rows[iter_dates + index_rnsupp * nb_dates]['prop'],
+                        'attentive': rows[iter_dates + index_attentive * nb_dates]['prop'],
+                        'general': rows[iter_dates + index_general * nb_dates]['prop'],
+                        'media': rows[iter_dates + index_media * nb_dates]['prop'],
+                    })
+                iter_dates += 1
 
 
-'''
-Note : il faudra aussi sorted par date les deputes supporters lorsque je pourrai pull après le pull request de BERTopic3
-
-Piste : ajout de party_day_counts = sorted(party_day_counts, key=lambda x: x[2]) dans script 02 ligne 217 pour trier par date et non plus pas parti les time series, mais des mofig
-Voir salade avec béatrice pour gérer ça (27/1)'''
+# Ouvre ton fichier pour compter les lignes et les colonnes
+file_path = os.path.join(args.origin_path, 'data_prod/var/bertopic/general_TS.csv')
