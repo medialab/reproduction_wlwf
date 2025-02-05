ab8b18c6c6fa46431e47fd6146f24bafadbeb790
jmeynent <fynch.meynent@sciencespo.fr>
2025-02-05 10:30:41 +0000

    Add argparse to make loading human annotation optionnal in 04_evaluate

---
+++ 04_evaluate_tm.py
@@ -0,0 +1,29 @@
+import openpyxl
+import pandas as pd 
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--load_annot",
+    help="If you want to create the dataframe for human annotation, use this option",
+    action = 'store_true',
+)
+args = parser.parse_args()
+
+if args.load_annot:
+    list_df = []
+    nb_annotateurs = 9
+
+    for i in range(nb_annotateurs):
+        df_annotateur = pd.read_excel('data_source/BERTopic_evaluation_collective.xlsx', sheet_name = i+1)
+        list_df.append(df_annotateur)
+    #Il faudra gérer ces duplicates 
+
+    df_merged = pd.concat(list_df, ignore_index=True) 
+    df_merged = df_merged.rename(columns={'Topic' : 'Topic_doc', 'pertinence' : 'pertinence_doc', 'coherence' : 'coherence_doc', 'Topic.1' : 'Topic_gen', 'pertinence.1' : 'pertinence_gen', 'coherence.1' : 'coherence_gen'})
+
+    df_merged[['Document', 'Topic_doc', 'Top_n_words', 'Representative_Docs', 'pertinence_doc', 'coherence_doc']].to_csv("data_prod/topics/bert-model/evaluation_doc_merged.csv")
+    df_merged[['Topic_gen', 'pertinence_gen', 'coherence_gen', 'nom']].to_csv("data_prod/topics/bert-model/evaluation_topic_merged.csv")
+
