4742dfeda9bd58ee8791d248f9e4c3e80d8c0019
jmeynent <fynch.meynent@sciencespo.fr>
2025-05-06 11:12:36 +0200

    correction

--- 03_evaluate_bertopic.py
+++ 03_evaluate_bertopic.py
@@ -3,6 +3,7 @@ from bertopic import BERTopic
 import os
 from sklearn.metrics import homogeneity_completeness_v_measure
 from sentence_transformers import SentenceTransformer
+import kaleido
 
 from utils import existing_dir_path, SBERT_NAME, reduce_doc_size, count_nb_files, preprocess
 
@@ -13,4 +14,4 @@ input_path = "/store/medialex/reproduction_wlwf/data_source/congress"
 docs = [doc for doc in preprocess(input_path, count_nb_files(input_path))]
 hierarchical_topics = topic_model.hierarchical_topics(docs)
 fig = topic_model.visualize_hierarchy(hierarchical_topics=hierarchical_topics)
-fig.write_html("data_prod/figures/hdbscan_hierarchy")
+fig.write_image("data_prod/figures/hdbscan_hierarchy.png")
