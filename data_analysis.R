2792da35aa56a533ecfa5718d8a1170a5db28948
jmeynent <fynch.meynent@sciencespo.fr>
2025-07-09 13:56:46 +0200

    Update stat des

--- data_analysis.R
+++ data_analysis.R
@@ -4,310 +4,63 @@ library(tidyr)
 library(ggplot2)
 library(reshape2)
 
-barbera <- read_csv("data_prod/figures/corrbarbera.csv", show_col_types=FALSE)
-ldanf <- read_csv("data_prod/figures/corrlda.csv", show_col_types=FALSE)
-ldaf <- read_csv("data_prod/figures/corrldafilter.csv", show_col_types=FALSE)
-
-names = c("barbera", "ldanf", "ldaf")
-i <-0
-
-db <- read_csv("data_prod/var/bertopic/issue-level/statio_details.csv",show_col_types=FALSE)
-titles <- read_csv("data_prod/figures/translate_number_name/BERTOPIC_merged.csv", ,show_col_types=FALSE)
+db <- read_csv("data_prod/var/general_TS.csv", show_col_types = FALSE)
+variables <- c('lr', 'majority', 'nupes', 'rn', 'lr_supp', 'majority_supp', 'nupes_supp', 'rn_supp', 'attentive', 'media')
+throw_topic <- c(16, 44, 54, 61, 64, 73, 76, 91, 1, 2, 5, 25, 41, 45, 3, 21, 26, 35, 50, 51, 56, 57, 58, 60, 65, 69, 78, 80, 87)
+pol_issues_temp <- setdiff(c(0:91), throw_topic)
+db <- db %>% mutate(topic = ifelse(topic == 89, 74, topic)) %>%
+  group_by(date, topic) %>%                                  
+  summarise(across(where(is.numeric), \(x) sum(x, na.rm = TRUE)), .groups = "drop") 
+pol_issues <- setdiff(pol_issues_temp, 89)  
+exclude_issues <- c(52, 71, 79, 85, 86)
+pol_issues <- setdiff(pol_issues, exclude_issues)
 
 db <- db %>%
-  mutate(n_OK = rowSums(across(everything(), ~ . == "OK")))
+  #dplyr::select(-general) %>%
+  filter(topic %in% pol_issues)
 
-db <- merge(db, titles, by="Topic", all.x = TRUE)
-
-write.csv(db, file="data_prod/var/bertopic/issue-level/statio_10.csv", row.names=FALSE)
-stop()
-for (mat in list(barbera, ldanf, ldaf)){
-    cormat <- as.matrix(mat[,-1])
-    cormat <- round(cormat,3)  
-    rownames(cormat) <- mat[[1]]
-    
-    # Melt pour ggplot
-    melted_cormat <- melt(cormat)
-    melted_cormat$Var1 <- factor(melted_cormat$Var1, levels = rev(rownames(cormat)))
-    melted_cormat$Var2 <- factor(melted_cormat$Var2, levels = colnames(cormat))
-    i <- i+1
-    
-    png(paste0("data_prod/figures/corrmat_",names[i],".png"),  width = 800, height = 800)
-    ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
-    geom_tile(color = "white")+
-    scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-    midpoint = 0, limit = c(-1,1), space = "Lab",
-    name="Pearson\nCorrelation") +
-    theme_minimal()+ # minimal theme
-    theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-        size = 12, hjust = 1))+
-    coord_fixed() 
-
-    ggfinal <- ggheatmap + 
-    geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-    theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank(),
-    axis.ticks = element_blank(),
-    legend.justification = c(1, 0),
-    legend.position.inside = c(0.6, 0.7),
-    legend.direction = "horizontal")+
-    guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                    title.position = "top", title.hjust = 0.5))
-    print(ggfinal)
-    dev.off()
+list_mat <- list()
+for (top in pol_issues){
+  db_topic <- db %>% filter (topic == top)
+  new_mat <- cor(db_topic[,variables])
+  list_mat <- c(list_mat, list(new_mat))
 }
 
 
-db <- read_csv("data_prod/var/bertopic/general_TS.csv", show_col_types = FALSE)
+mat <- Reduce("+", list_mat) / length(list_mat)
+#Correlation matrix 
+cormat <- round(mat,3)  
+rownames(cormat) <- variables
 
-variables <- c('lr', 'majority', 'nupes', 'rn', 'lr_supp', 'majority_supp', 'nupes_supp', 'rn_supp', 'attentive', 'general', 'media')
-
-#pol_issues <- c(0, 1, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 22, 27, 28, 29, 30, 31, 32, 33, 34, 36, 
-    #37, 38, 39, 40, 42, 43, 44, 45, 46, 47, 48, 49, 52, 53, 54, 55, 58, 59, 62, 63, 64, 66, 67, 68, 70, 71, 
-    #72, 73, 74, 75, 77, 80, 81, 82, 83, 84, 85, 86, 89, 90, 91) 
-
-pol_issues <- c(0:91)
-
-db <-  db %>%
-    filter(topic %in% pol_issues)
-
-db_binary <- db
-db_binary[,variables] <- sapply(db_binary[, variables], function(x) ifelse(x > 0, 1, 0))
-print(sample_n(db_binary, 30))
-
-get_pairwise_identical_proportions <- function(data, variables) {
-  # Initialiser une matrice vide pour les résultats
-  proportions <- matrix(NA, nrow = length(variables), ncol = length(variables))
-  rownames(proportions) <- variables
-  colnames(proportions) <- variables
-  
-  # Boucle pour comparer chaque couple de variables
-  for (i in 1:(length(variables) - 1)) {
-    for (j in (i + 1):length(variables)) {
-      # Extraire les valeurs des deux variables
-      var1 <- data[[variables[i]]]
-      var2 <- data[[variables[j]]]
-      
-      # Comparer les valeurs de var1 et var2 pour trouver les valeurs identiques
-      match_identical <- sum(var1 == var2) 
-      total <- length(var1)               
-      proportions[i, j] <- round(match_identical / total,2) 
-      proportions[j, i] <- round(match_identical / total,2)
-      proportions[i, i] <- 1
-      proportions[j, j] <- 1
-    }
-  }
-  
-  # Retourner la matrice des proportions
-  return(proportions)
-}
-
-proportion_table <- get_pairwise_identical_proportions(db_binary, variables)
-melted_proportions <- melt(proportion_table)
-melted_proportions$value <- as.numeric(melted_proportions$value)
-print(melted_proportions)
-png("data_prod/var/bertopic/corrmat_logit.png",  width = 800, height = 800)
-ggheatmap <- ggplot(melted_proportions, aes(Var2, Var1, fill = value)) +
-    geom_tile(color = "white")+
- scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-   midpoint = 0, limit = c(0,1), space = "Lab",
-   name="Proportion of identical logit variable between groups") +
-  theme_minimal()+ # minimal theme
- theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-    size = 12, hjust = 1))+
- coord_fixed()  + 
-geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-theme(
-  axis.title.x = element_blank(),
-  axis.title.y = element_blank(),
-  panel.grid.major = element_blank(),
-  panel.border = element_blank(),
-  panel.background = element_blank(),
-  axis.ticks = element_blank(),
-  legend.justification = c(1, 0),
-  legend.position.inside = c(0.6, 0.7),
-  legend.direction = "horizontal")+
-  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                title.position = "top", title.hjust = 0.5))
-print(ggheatmap)
-dev.off()
-
-
-
-drop_top = c()
-for (i in unique(db$topic)){
-      pdb_top <- db %>% filter(topic==i)
-      for (v in variables){
-        if (sd(pdb_top[[v]]) == 0){
-          drop_top <- c(drop_top, i)
-        }
-      }
-    }
-
-db <- db %>% filter (!(topic %in% unique(drop_top)))
-
-
-# - logit transform all series
-for (v in variables) {
-# - pulling the series-agenda for that group
-    db[[v]] <- as.numeric(db[[v]])
-    x <- db[,v]
-
-    if (any(x > 0.998)) { #Treat the case of a proportion value >= 1 (x can be > 1 because of the previous line). We chose 0.999 because rounding of 0.9999 is equal to 1 in db 
-        for (i in 1:nrow(x)) {
-        if (db[i, v] > 0.998) {
-            print(paste("WARNING: due to proportion value equal to 1 or almost equal to 1,  small transformation has been applied for a value in the following group :", v))
-            db[i, v] <- 0.998
-            }
-        }
-        }
-
-    x <- db[,v]
-    # - adding 1 percentage point to avoid 0s before the logit transformation
-    x <- x + 0.001
-
-    # - applying the non-linear transformation
-    logit_x <- log(x / (1 - x))
-    db[,v] <- logit_x
-}
-
-db_corr <- db %>% select(variables) 
-cormat <- round(cor(db_corr),2)
+# Melt pour ggplot
 melted_cormat <- melt(cormat)
-melted_cormat$Var1 <- factor(melted_cormat$Var1, levels = rev(colnames(db_corr)))
-png("data_prod/figures/bertopic/corrmat_day.png",  width = 800, height = 800)
+melted_cormat$Var1 <- factor(melted_cormat$Var1, levels = rev(rownames(cormat)))
+melted_cormat$Var2 <- factor(melted_cormat$Var2, levels = colnames(cormat))
 
+png(paste0("data_prod/figures/topic_corrmat.png"),  width = 800, height = 800)
 ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
- geom_tile(color = "white")+
- scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-   midpoint = 0, limit = c(-1,1), space = "Lab",
-   name="Pearson\nCorrelation") +
-  theme_minimal()+ # minimal theme
- theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+geom_tile(color = "white")+
+scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+midpoint = 0, limit = c(-1,1), space = "Lab",
+name="Pearson\nCorrelation") +
+theme_minimal()+ # minimal theme
+theme(axis.text.x = element_text(angle = 45, vjust = 1, 
     size = 12, hjust = 1))+
- coord_fixed() 
+coord_fixed() 
 
-ggheatmap + 
+ggfinal <- ggheatmap + 
 geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
 theme(
-  axis.title.x = element_blank(),
-  axis.title.y = element_blank(),
-  panel.grid.major = element_blank(),
-  panel.border = element_blank(),
-  panel.background = element_blank(),
-  axis.ticks = element_blank(),
-  legend.justification = c(1, 0),
-  legend.position.inside = c(0.6, 0.7),
-  legend.direction = "horizontal")+
-  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+axis.title.x = element_blank(),
+axis.title.y = element_blank(),
+panel.grid.major = element_blank(),
+panel.border = element_blank(),
+panel.background = element_blank(),
+axis.ticks = element_blank(),
+legend.justification = c(1, 0),
+legend.position.inside = c(0.6, 0.7),
+legend.direction = "horizontal")+
+guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
                 title.position = "top", title.hjust = 0.5))
+print(ggfinal)
 dev.off()
-
-  
-db$topic <- as.character(db$topic)
-db_bis <- db %>%
-    filter(date > as.Date("2022-06-21")) %>% #Remove the two first days to have a number of days divisible by 7. 
-    group_by(topic) %>%
-    mutate(group = ceiling(row_number() /7)) %>% 
-    group_by(group, topic) %>%
-    summarise(date = first(group), across(where(is.numeric), mean), .groups='drop') %>%
-    arrange(as.numeric(topic)) 
-
-write.csv(db_bis, "data_prod/var/bertopic/nodiff_week.csv" ,row.names = FALSE)
-
-db_corr <- db_bis %>% select(variables) 
-cormat <- round(cor(db_corr),2)
-melted_cormat <- melt(cormat)
-melted_cormat$Var1 <- factor(melted_cormat$Var1, levels = rev(colnames(db_corr)))
-png("data_prod/figures/bertopic/corrmat_week.png",  width = 800, height = 800)
-
-ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
- geom_tile(color = "white")+
- scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-   midpoint = 0, limit = c(-1,1), space = "Lab",
-   name="Pearson\nCorrelation") +
-  theme_minimal()+ # minimal theme
- theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-    size = 12, hjust = 1))+
- coord_fixed() 
-
-ggheatmap + 
-geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-theme(
-  axis.title.x = element_blank(),
-  axis.title.y = element_blank(),
-  panel.grid.major = element_blank(),
-  panel.border = element_blank(),
-  panel.background = element_blank(),
-  axis.ticks = element_blank(),
-  legend.justification = c(1, 0),
-  legend.position.inside = c(0.6, 0.7),
-  legend.direction = "horizontal")+
-  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                title.position = "top", title.hjust = 0.5))
-dev.off()
-
-print("start boucle")
-
-for (i in 2:6){
-    if(i==3){
-        db_bis <- db %>%
-        filter(date > as.Date("2022-06-20")) 
-    } else if (i==5){
-        db_bis<- db %>%
-        filter(date > as.Date("2022-06-22")) 
-    } else if (i==6){
-        db_bis <- db %>%
-        filter(date > as.Date("2022-06-23")) 
-    } else {
-        db_bis <- db 
-    }
-
-    db_bis <- db_bis %>%  #Remove the two first days to have a number of days divisible by 7. 
-    group_by(topic) %>%
-    mutate(group = ceiling(row_number() /i)) %>% 
-    group_by(group, topic) %>%
-    summarise(date = first(group), across(where(is.numeric), mean), .groups='drop') %>%
-    arrange(as.numeric(topic)) 
-
-    db_corr <- db_bis %>% select(variables)
-
-    cormat <- round(cor(db_corr),2)
-    melted_cormat <- melt(cormat)
-    melted_cormat$Var1 <- factor(melted_cormat$Var1, levels = rev(colnames(db_corr)))
-
-    png(paste0("data_prod/figures/bertopic/corrmat_", i, "day.png"),  width = 800, height = 800)
-
-    ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
-    geom_tile(color = "white")+
-    scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-        midpoint = 0, limit = c(-1,1), space = "Lab",
-        name="Pearson\nCorrelation") +
-    theme_minimal()+ # minimal theme
-    theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-        size = 12, hjust = 1))+
-    coord_fixed() 
-
-    final_plot <- ggheatmap + 
-    geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-    theme(
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_blank(),
-        axis.ticks = element_blank(),
-        legend.justification = c(1, 0),
-        legend.position.inside = c(0.6, 0.7),
-        legend.direction = "horizontal")+
-        guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                        title.position = "top", title.hjust = 0.5))
-    
-    print(final_plot)
-    dev.off()
-    }
-
