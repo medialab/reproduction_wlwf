7fca7fe675551957c98e9dce1d5470e14f6e18e8
bmaz <beatrice.mazoyer@gmail.com>
2025-12-19 20:50:14 +0100

    Fix get_suspended_users.sh

--- scripts/get_suspended_users.sh
+++ scripts/get_suspended_users.sh
@@ -1,9 +1,6 @@
 # Use the undocumented Twitter API endpoint cdn.syndication.twimg.com to get info about suspended users
 # xan version 0.54.1
-# minet version
-
-
-pyenv activate minet
+# minet version 4.2.1
 
 xan cat rows /store/medialex/v3_data_reproduction_wlwf/data_source/attentive/*.csv | \
 xan dedup -s user_id | \
@@ -11,9 +8,9 @@ xan select user_id,user_name,user_screen_name,user_followers,user_description,id
 xan map 'concat("https://cdn.syndication.twimg.com/tweet-result?id=",id,"&token=123") as frame_url' | \
 minet fetch frame_url -i - | \
 xan select user_id,user_name,user_screen_name,user_followers,user_description,frame_url,mimetype,path \
-> attentive_users_fetch_report.csv
+> /store/medialex/v3_data_reproduction_wlwf/attentive_users_fetch_report.csv
 
 xan search -s mimetype "application/json" attentive_users_fetch_report.csv | \
 xan map -p '"downloaded".pjoin(path).read_json().tombstone as json' | \
 xan search -s json "suspended account" | \
-xan map 'json.parse_json().text as json_text' > attentive_users_suspended.csv
+xan map 'json.parse_json().text as json_text' > /store/medialex/v3_data_reproduction_wlwf/attentive_users_suspended.csv
