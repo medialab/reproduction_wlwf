a43680484fb335a46e3160412d2b2ca960d094bc
bmaz <beatrice.mazoyer@gmail.com>
2025-12-19 20:57:18 +0100

    Save to /store

--- scripts/get_suspended_users.sh
+++ scripts/get_suspended_users.sh
@@ -6,7 +6,7 @@ xan cat rows /store/medialex/v3_data_reproduction_wlwf/data_source/attentive/*.c
 xan dedup -s user_id | \
 xan select user_id,user_name,user_screen_name,user_followers,user_description,id | \
 xan map 'concat("https://cdn.syndication.twimg.com/tweet-result?id=",id,"&token=123") as frame_url' | \
-minet fetch frame_url -i - | \
+minet fetch frame_url -i - -O /store/medialex/v3_data_reproduction_wlwf/downloaded_attentive_users | \
 xan select user_id,user_name,user_screen_name,user_followers,user_description,frame_url,mimetype,path \
 > /store/medialex/v3_data_reproduction_wlwf/attentive_users_fetch_report.csv
 
