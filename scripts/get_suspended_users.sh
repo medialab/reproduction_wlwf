9b7f027667dfe4e5e9a3aeb0efe6fc19a719cbed
bmaz <beatrice.mazoyer@gmail.com>
2025-12-29 12:03:29 +0100

    Fix path

--- scripts/get_suspended_users.sh
+++ scripts/get_suspended_users.sh
@@ -10,7 +10,7 @@ minet fetch frame_url -i - -O /store/medialex/v3_data_reproduction_wlwf/download
 xan select user_id,user_name,user_screen_name,user_followers,user_description,frame_url,mimetype,path \
 > /store/medialex/v3_data_reproduction_wlwf/attentive_users_fetch_report.csv
 
-xan search -s mimetype "application/json" attentive_users_fetch_report.csv | \
+xan search -s mimetype "application/json" /store/medialex/v3_data_reproduction_wlwf/attentive_users_fetch_report.csv | \
 xan map -p '"/store/medialex/v3_data_reproduction_wlwf/downloaded_attentive_users".pjoin(path).read_json().tombstone as json' | \
 xan search -s json "suspended account" | \
 xan map 'json.parse_json().text as json_text' > /store/medialex/v3_data_reproduction_wlwf/attentive_users_suspended.csv
