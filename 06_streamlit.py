aeac5f96c18c9437b9b60d2f01e405415a4b743b
bmaz <beatrice.mazoyer@gmail.com>
2026-01-20 16:03:58 +0100

    Improve chart legend

--- 06_streamlit.py
+++ 06_streamlit.py
@@ -9,6 +9,27 @@ import plotly.express as px
 
 from utils import existing_dir_path
 
+color_palette = {
+    "majority": "#ffd16a",
+    "lr": "#0068C9",
+    "nupes": "#ff2b2b",
+    "rn": "#6d3fc0",
+    "media": "#29b09d",
+    "majority_supp": "#ff8700",
+    "lr_supp": "#83c9ff",
+    "nupes_supp": "#ffabab",
+    "rn_supp": "mediumpurple",
+    "attentive": "#d5dae5",
+}
+
+public_types = {
+    "députés": ["majority", "lr", "nupes", "rn"],
+    "supporters": ["majority_supp", "lr_supp", "nupes_supp", "rn_supp"],
+    "médias": ["media"],
+    "public attentif": ["attentive"],
+}
+
+display_strings = {"congress": "députés", "media": "médias"}
 
 parser = argparse.ArgumentParser()
 
@@ -69,7 +90,7 @@ def render_tweets(df, height=1200):
                 """)
         columns.append(f"""
         <div class="tweet-column">
-            <h3>{public.capitalize()}</h3>
+            <h4>Tweets représentatifs des {display_strings[public]}</h3>
             {"".join(blocks)}
         </div>
         """)
@@ -136,11 +157,34 @@ with st.container():
     )
     ts_data["date"] = pd.to_datetime(ts_data["date"])
     # st.line_chart(ts_data, x="date", y="prop", color="party")
+    options = list(public_types.keys())
 
-    fig = px.line(ts_data, x="date", y="prop", color="party")
-    fig.update_layout(
-        legend=dict(orientation="h", yanchor="bottom", y=-0.35, xanchor="center", x=0.5)
+    selection = st.pills("", options, selection_mode="multi", default="députés")
+    selected_publics = []
+    for sel in selection:
+        selected_publics.extend(public_types[sel])
+    ts_data = ts_data[ts_data.party.isin(selected_publics)]
+
+    fig = px.line(
+        ts_data,
+        x="date",
+        y="prop",
+        color="party",
+        height=300,
+        color_discrete_sequence=list(color_palette.values()),
+        category_orders={"party": list(color_palette.keys())},
     )
+    fig.update_layout(
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=-0.5,
+            xanchor="center",
+            x=0.5,
+            title=None,
+        )
+    )
+    fig.update_xaxes(title=None)
 
     st.plotly_chart(
         fig,
